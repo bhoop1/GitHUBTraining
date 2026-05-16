8c6c2027e26d65e6d8b1e194b9f211b37b56dfdd
Khemchand Singh <1khemchand@gmail.com>
2026-05-16 14:26:46 +0530

    Added isAbove 18 fucntion

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const age = 34;
-const name = "John Doe";
+const name1 = "John Doe";
 const dob = "01/01/1990";
 
 
-function sayName() {  console.log(`My name is ${name}`);
+function sayName() {  console.log(`My name is ${name1}`);
+}
+
+function isAbove18() {
+    return age > 18;
 }
